54529321e5e550d5c21df321ba5704ad2d48a7af
Ryusaem <53605044+Ryusaem@users.noreply.github.com>
2024-08-01 22:32:54 +0200

    Ending the mobile side of the project

--- js/script.js
+++ js/script.js
@@ -4,8 +4,11 @@ const menuContainerClose = document.querySelector(".menu-container__close");
 
 const previousButton = document.querySelector(".galery-container__previous");
 const nextButton = document.querySelector(".galery-container__next");
-
 let currentImage = document.querySelector(".galery-container__image");
+
+const minusButton = document.querySelector(".range-container__minus");
+const plusButton = document.querySelector(".range-container__plus");
+const rangeValue = document.querySelector(".range-container__range-value");
 
 function toggleMenu() {
   menuContainer.classList.toggle("active");
@@ -30,9 +33,21 @@ function previousImage() {
   }.jpg`;
 }
 
+function addElement() {
+  rangeValue.textContent = +rangeValue.textContent + 1;
+}
+
+function removeElement() {
+  rangeValue.textContent =
+    rangeValue.textContent === "0" ? 0 : +rangeValue.textContent - 1;
+}
+
 // Event Listeners
 mobileMenu.addEventListener("click", toggleMenu);
 menuContainerClose.addEventListener("click", toggleMenu);
 
 previousButton.addEventListener("click", previousImage);
 nextButton.addEventListener("click", nextImage);
+
+minusButton.addEventListener("click", removeElement);
+plusButton.addEventListener("click", addElement);
